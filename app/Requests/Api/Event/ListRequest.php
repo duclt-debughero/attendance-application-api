b1f8f22d82a8b35ce93b65baf389ecc0d501c8d9
Lê Trí Đức <ltd.duc.io@gmail.com>
2025-01-11 23:26:56 +0700

    refs #100098 [Coding] API Event Export CSV

---
+++ app/Requests/Api/Event/ListRequest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Requests\Api\Event;
+
+use App\Libs\ConfigUtil;
+use App\Requests\Api\BaseApiRequest;
+use App\Rules\FormatDate;
+use Illuminate\Http\Request;
+
+class ListRequest extends BaseApiRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(Request $request) {
+        return [
+            'event_start_time_from' => [
+                'nullable',
+                new FormatDate('Y/m/d'),
+            ],
+            'event_start_time_to' => [
+                'nullable',
+                new FormatDate('Y/m/d'),
+                'after_or_equal:event_start_time_from',
+            ],
+            'event_end_time_from' => [
+                'nullable',
+                new FormatDate('Y/m/d'),
+            ],
+            'event_end_time_to' => [
+                'nullable',
+                new FormatDate('Y/m/d'),
+                'after_or_equal:event_end_time_from',
+            ],
+        ];
+    }
+
+    /**
+     * Retrieves the attributes of the user model.
+     *
+     * @return array An associative array with the keys:
+     */
+    public function attributes() {
+        return [
+            'event_start_time_from' => 'event_start_time_from',
+            'event_start_time_to' => 'event_start_time_to',
+            'event_end_time_from' => 'event_end_time_from',
+            'event_end_time_to' => 'event_end_time_to',
+        ];
+    }
+
+    /**
+     * Validation error messages.
+     *
+     * @return array
+     */
+    public function messages() {
+        $parentMessage = parent::messages();
+
+        return array_merge($parentMessage, [
+            'event_start_time_to.after_or_equal' => ConfigUtil::getMessage('ECL026', ['event_start_time_to', 'event_start_time_from']),
+            'event_end_time_to.after_or_equal' => ConfigUtil::getMessage('ECL026', ['event_end_time_to', 'event_end_time_from']),
+        ]);
+    }
+}
